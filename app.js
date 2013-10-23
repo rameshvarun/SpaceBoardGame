e061b59968d1791d01243e644c329cd9152ecedd
Varun Ramesh <varun.ramesh@aol.com>
2013-10-22 22:47:27 -0700

    Trying to figure out Facebook authentication.

--- app.js
+++ app.js
@@ -4,18 +4,47 @@ var http = require('http');
 var path = require('path');
 var swig = require('swig');
 var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
 var socketio = require('socket.io');
+var db = require('./db');
+var globals = require('./globals');
 
 var app = express();
 
-//All environments
-app.set('port', process.env.PORT || 3000);
+var keys = require('./keys.json');
+
+passport.use(new FacebookStrategy({
+    clientID: keys.FACEBOOK_APP_ID,
+    clientSecret: keys.FACEBOOK_APP_SECRET,
+    callbackURL: globals.APP_URL + "/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log(profile);
+	
+  }
+));
+
+app.use(express.logger('dev')); //Logging
+app.use(express.bodyParser()); //Parses form data
+app.use(express.favicon()); //Favicon
+
+//Register Swig as template parser
+app.engine('html', swig.renderFile);
+app.set('views', __dirname + '/views');
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
 
 //Serve static files from public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+//All the application routes
+
+//Authentication routes
+app.get('/auth/facebook', passport.authenticate('facebook')); //Let's users login to Facebook
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' })); //Facebook redirects users here after they log in
+
 //Start server on the correct port number
 var server = http.createServer(app);
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+server.listen( globals.PORT, function(){
+  console.log('Express server listening on port ' + globals.PORT);
 });
