a3871e7bd0092ed9ba90a7ec26e1c14f118269f3
Varun Ramesh <varun.ramesh@aol.com>
2013-11-04 11:02:42 -0800

    Better way to manage keys

--- globals.js
+++ globals.js
@@ -1,6 +1,6 @@
 exports.PORT = process.env.PORT || 80;
 exports.APP_URL = process.env.URL || "http://localhost";
 
-exports.FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
-exports.FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
-exports.SESSION_TOKEN = process.env.SESSION_TOKEN;
+exports.FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID || require('./keys').FACEBOOK_APP_ID;
+exports.FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET || require('./keys').FACEBOOK_APP_SECRET;
+exports.SESSION_TOKEN = process.env.SESSION_TOKEN || require('./keys').SESSION_TOKEN;
