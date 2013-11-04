e5f1245ddce9a42d918a80a42cfbcf2b9d24db81
Varun Ramesh <varun.ramesh@aol.com>
2013-11-04 14:31:32 -0800

    Test

--- globals.js
+++ globals.js
@@ -1,6 +1,6 @@
 exports.PORT = process.env.PORT || 80;
 exports.APP_URL = process.env.URL || "http://localhost";
 
-exports.FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID || require('./keys').FACEBOOK_APP_ID;
-exports.FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET || require('./keys').FACEBOOK_APP_SECRET;
-exports.SESSION_TOKEN = process.env.SESSION_TOKEN || require('./keys').SESSION_TOKEN;
+exports.FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID || "167857050088800";
+exports.FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET || "5540e32e065dbc9352be38b9160d1c2d";
+exports.SESSION_TOKEN = process.env.SESSION_TOKEN || "qELLFGB2P0Xoac3e6Uf641luwvoNfHvOrFFbm8bmcFWi7IKD9IcMhqBjMyV3sYP";
